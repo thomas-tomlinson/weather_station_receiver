5e4f28511654d13af6868663791af6739c6eebc9
Thomas Tomlinson <thomas_tomlinson@mac.com>
2024-05-23 19:11:42 -0600

    added ntp time sync, asyncio awaits in inner uart loop

--- main.py
+++ main.py
@@ -4,11 +4,21 @@ import time
 import asyncio
 import binascii
 from microdot import Microdot
+import ntptime
 
 app = Microdot()
 currentWeatherData = {}
 # hc-12 radio setup
 uart2 = UART(2, baudrate=9600, tx=17, rx=16)
+
+# try to get the time
+while True:
+    try:
+        ntptime.settime()
+        break
+    except OSError:
+        print("couldn't sync ntp, waiting 2 seconds")
+        time.sleep(2)
 
 def processPayload(payload):
     filtered_data = payload.replace(b'\xff', b'')
@@ -24,7 +34,11 @@ def processPayload(payload):
         print('decode error: ', e, 'payload: ', filtered_data)
         return None
 
+    mytime = time.gmtime()
+    iso8601 = str("{}-{:0>2}-{:0>2}T{:0>2}:{:0>2}:{:0>2}Z".format(mytime[0], mytime[1], mytime[2], mytime[3], mytime[4], mytime[5])) 
+    weather_data['recordtime'] = iso8601
     return weather_data
+
 
 async def flash_led():
     led_pin = Pin(23, Pin.OUT)
@@ -41,6 +55,7 @@ async def uart_listener():
             stopCharReached = False
             # we need to read up to the first new line, then process
             while stopCharReached is False:
+                await asyncio.sleep(0.1)
                 char = uart2.read(1)
                 if char is None:
                     continue
