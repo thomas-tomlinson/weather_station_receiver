eff808b947423146fe2a96125e27846f1cbe1c60
Thomas Tomlinson <thomas_tomlinson@mac.com>
2024-06-06 19:12:41 -0600

    websocket server along with root query page.  all units standardized to us for processing

--- main.py
+++ main.py
@@ -3,52 +3,61 @@ import bme280_float as bme280
 import json
 import time
 import asyncio
-import binascii
 from microdot import Microdot
+from microdot.websocket import with_websocket
 import ntptime
 import network
 import esp
 from struct import unpack
 
 app = Microdot()
-#currentWeatherData = {}
-## hc-12 radio setup
-#uart2 = UART(2, baudrate=9600, tx=17, rx=16)
-#i2c = I2C(0, scl=Pin(22), sda=Pin(21))
-#bme = bme280.BME280(i2c=i2c)
+currentWeatherData = {}
+uart2 = UART(2, baudrate=9600, tx=17, rx=16)
+i2c = I2C(0, scl=Pin(22), sda=Pin(21))
+bme = bme280.BME280(i2c=i2c)
+event = asyncio.Event()
+
 esp.osdebug(True)
-#app = Microdot()
+
 def iso8601():
     mytime = time.gmtime()
     iso8601 = str("{}-{:0>2}-{:0>2}T{:0>2}:{:0>2}:{:0>2}Z".format(mytime[0], mytime[1], mytime[2], mytime[3], mytime[4], mytime[5]))
     return iso8601
 
 def processPayload(payload):
-    filtered_data = payload
-    #filtered_data = payload.replace(b'\xff', b'')
-    #if len(filtered_data) == 0:
-        # empty data, most likely from noise on the radio
-    #    return None
-
-    #try:
-        #decoded = binascii.a2b_base64(filtered_data)
-    decoded = unpack_data(filtered_data)
+    decoded = unpack_data(payload)
     if decoded is None:
-        print('unpack error of payload: {}'.format(filtered_data))
+        print('unpack error of payload: {}'.format(payload))
         return None
+    # convert to final values.  mostly metric to US but also wind and rain to real units
+    decoded['rainbuckets'] = process_rain_buckets(decoded['rainbuckets'])
+    decoded['avg_wind'] = process_anemometer(decoded['avg_wind'])
+    decoded['gust_wind'] = process_anemometer(decoded['avg_wind'])
+    decoded['temp'] = c_to_f(decoded['temp'])
+    decoded['pressure'] = pascal_to_inhg(decoded['pressure'])
+    decoded['recordtime'] = iso8601()
+    return decoded
 
-    #weather_data = json.loads(decoded)
-    weather_data = decoded
-        
-    #except ValueError as e:
-    #    print('decode error: ', e, 'payload: ', filtered_data)
-    #    return None
+def process_rain_buckets(count):
+    # 125 -ish counts is 30mm, = .24mm a count
+    total_inches = (count * 0.009)
+    return total_inches
 
-    #mytime = time.gmtime()
-    #iso8601 = str("{}-{:0>2}-{:0>2}T{:0>2}:{:0>2}:{:0>2}Z".format(mytime[0], mytime[1], mytime[2], mytime[3], mytime[4], mytime[5]))
-    weather_data['recordtime'] = iso8601()
-    #print("weather_data payload: {}".format(weather_data))
-    return weather_data
+def process_anemometer(count):
+    # anemometer factor is 3.2.  for now, just do a linear calc without a model
+    anemometer_factor = 3.2
+    mmps = (2 * 3.14 * count * 85 * anemometer_factor)
+    mps = (mmps / 1000)
+    mph = (mps * 2.237)  
+    return mph
+
+def c_to_f(temp):
+    f = 32 + (temp * 9/5)
+    return f
+
+def pascal_to_inhg(pascal):
+    inhg = (pascal / 3386) 
+    return inhg
 
 def unpack_data(payload):
     try:
@@ -77,43 +86,43 @@ async def flash_led():
     led_pin.off()
 
 async def uart_listener():
-    global currentWeatherData
     sreader = asyncio.StreamReader(uart2)
     while True: 
-        #await asyncio.sleep(1)
-
-        #if uart2.any() > 0:
-        #    readbuf = b''
-            #stopCharReached = False
-            # we need to read up to the first new line, then process
-            #while stopCharReached is False:
-            #    await asyncio.sleep(0.1)
-            #    char = uart2.read(1)
-            #    if char is None:
-            #        continue
-            #    if char == b'\n':
-            #        #break character reached, end this
-            #        stopCharReached = True
-            #    else:
-            #        readbuf = readbuf + char
-            #readbuf = uart2.readline()
-            #processedData = processPayload(readbuf)
-        #readbuf = await sreader.readline()
         readbuf = await sreader.read(33)
-        processedData = processPayload(readbuf)
+        remote_data = processPayload(readbuf)
 
-        if processedData is not None:
-            currentWeatherData['remote'] = processedData
-            currentWeatherData['local'] = read_bme280()
-            # flash the status LED to indicate we received good data
-            print(currentWeatherData)
-            asyncio.create_task(flash_led())
+        if remote_data is None:
+            continue
+
+        update_weather_data(remote_data)
+        event.set()
+        asyncio.create_task(flash_led()) # should be in update_weather_data
+
+        #    currentWeatherData['remote'] = processedData
+        #    currentWeatherData['local'] = read_bme280()
+        #    # flash the status LED to indicate we received good data
+        #    print(currentWeatherData)
+        #    asyncio.create_task(flash_led())
+
+def update_weather_data(remote_data):
+    global currentWeatherData
+    currentWeatherData['remote'] = remote_data
+    currentWeatherData['local'] = read_bme280()
+    currentWeatherData['recordtime'] = iso8601()
+
+def retrieve_weather_data(format=None):
+    global currentWeatherData
+    if format == "json":
+        return json.dumps(currentWeatherData)        
+    else:
+        return currentWeatherData
+
 
 def read_bme280():
     dict = {}
-    rawread = bme.values
-    dict['temp'] = rawread[0]
-    dict['pressure'] = rawread[1]
+    rawread = bme.read_compensated_data()
+    dict['temp'] = c_to_f(rawread[0])
+    dict['pressure'] = pascal_to_inhg(rawread[1])
     dict['humidity'] = rawread[2]
     dict['recordtime'] = iso8601()
     return dict
@@ -133,25 +142,26 @@ def config_hc12():
 
 @app.route('/')
 async def index(request):
-    global currentWeatherData
-    currentWeatherData['requesttime'] = iso8601()
-    return currentWeatherData
+    wdata = retrieve_weather_data()
+    wdata['requesttime'] = iso8601()
+    return wdata
+
+@app.route('/weather_stream')
+@with_websocket
+async def weather_stream(request, ws):
+        while True:
+            await event.wait() 
+            wdata = retrieve_weather_data(format='json')
+            await ws.send(wdata)
+            event.clear()
 
 async def main():
+    config_hc12()
     asyncio.create_task(uart_listener())
     await app.start_server()
-    #while True:
-    #    await asyncio.sleep_ms(100)
 
-currentWeatherData = {}
-# hc-12 radio setup
-uart2 = UART(2, baudrate=9600, tx=17, rx=16)
-i2c = I2C(0, scl=Pin(22), sda=Pin(21))
-bme = bme280.BME280(i2c=i2c)
 
-config_hc12()
-
-# try to get the time
+# ensure that time is set
 while True:
     try:
         ntptime.settime()
@@ -159,7 +169,6 @@ while True:
     except OSError:
         print("couldn't sync ntp, waiting 2 seconds")
         time.sleep(2)
-# hc12 setup
 
 asyncio.run(main())
 main()
