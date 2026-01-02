70373c9f9a6b02fc952a791e766ed9967f8c0867
Thomas Tomlinson <thomas_tomlinson@mac.com>
2026-01-02 12:39:44 -0700

    removed old code

--- src/main.py
+++ src/main.py
@@ -42,7 +42,6 @@ def processPayload(payload):
     decoded = update_value(decoded, 'avg_wind', process_anemometer)
     decoded = update_value(decoded, 'gust_wind', process_anemometer)
     decoded = update_value(decoded, 'temp', c_to_f)
-    #decoded = update_value(decoded, 'pressure', pascal_to_inhg)
     decoded = update_value(decoded, 'wind_dir', reverse_wind_dir)
     print("processed packet: {}".format(decoded))
     return decoded
@@ -63,7 +62,7 @@ def verify_payload(bytes):
     try:
         checksum1, checksum2 = unpack(">hh", checksum)
     except ValueError:
-        raise ValueError('failed to unpack checksum') 
+        raise ValueError('failed to unpack checksum')
 
     data = bytes[0:0-4:]
     datasum = sum(data)
@@ -94,16 +93,16 @@ def reverse_wind_dir(winddir):
 
 def process_rain_buckets(count):
     # 115.3 pulses for is 30mm.  0.26mm / pulse
-    # 30 mm = 1.18inches. 1.18 / 115.3 = 0.01 inches a pulse 
+    # 30 mm = 1.18inches. 1.18 / 115.3 = 0.01 inches a pulse
     total_inches = (count * 0.01)
     return total_inches
 
 def process_anemometer(count):
-    # anemometer factor 
+    # anemometer factor
     anemometer_factor = 5.123 # cones with 5mm lip, 79mm
     mmps = (2 * 3.14 * count * 79 * anemometer_factor)
     mps = (mmps / 1000)
-    mph = (mps * 2.237)  
+    mph = (mps * 2.237)
     return mph
 
 def c_to_f(temp):
@@ -111,7 +110,7 @@ def c_to_f(temp):
     return f
 
 def pascal_to_inhg(pascal):
-    inhg = (pascal / 3386) 
+    inhg = (pascal / 3386)
     return inhg
 
 async def flash_led():
@@ -120,34 +119,18 @@ async def flash_led():
     await asyncio.sleep_ms(500)
     led_pin.off()
 
-class stream_observer:
-    def update(self, data: bytes) -> None:
-        ic = ord(data[:1])
-        if ic == 0xC4:
-            print('found header, sleeping 100ms')
-            time.sleep_ms(100)
-        print(f'{data}')
-
 async def uart_listener():
     print('starting UART listener')
-    #uart_aloader = umsgpack.aloader(asyncio.StreamReader(uart2), observer=stream_observer())
     sreader = asyncio.StreamReader(uart2)
     buf = bytes()
-    while True: 
+    while True:
         res = await sreader.read(1)
         if ord(res) == 0xc4:
             await asyncio.sleep_ms(500)
             length = await sreader.read(1)
             length = ord(length)
             buf = await sreader.read(length)
-            handle_data(buf) 
-#        try:
-#            res = await uart_aloader.load()
-#        except Exception as e:
-#            print("aloader failed to process: {}".format(e))
-#            continue
-#        print("mem free: {}".format(gc.mem_free()))
-#        handle_data(res)
+            handle_data(buf)
 
 def handle_data(data):
     remote_data = processPayload(data)
@@ -167,7 +150,7 @@ def publish_mqtt(publish_payload):
         return None
     topic = b'esp32_weather_feed'
     send_data = b''
-    send_data = send_data + publish_payload 
+    send_data = send_data + publish_payload
     mqtt.publish(topic, send_data)
     print("published mqtt message")
     mqtt.disconnect()
@@ -182,7 +165,7 @@ def retrieve_weather_data(format=None):
     global currentWeatherData
     currentWeatherData['requesttime'] = iso8601()
     if format == "json":
-        return json.dumps(currentWeatherData)        
+        return json.dumps(currentWeatherData)
     else:
         return currentWeatherData
 
@@ -195,7 +178,7 @@ def read_bme280():
     return dict
 
 def config_hc12():
-    # init the HC-12 
+    # init the HC-12
     set_pin = Pin(26, Pin.OUT)
     set_pin.off()
     time.sleep_ms(200)
@@ -217,7 +200,7 @@ async def index(request):
 @with_websocket
 async def weather_stream(request, ws):
         while True:
-            await event.wait() 
+            await event.wait()
             wdata = retrieve_weather_data(format='json')
             await ws.send(wdata)
             event.clear()
